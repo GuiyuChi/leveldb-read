c16af783d38d872a1ff8771215fd736ecc102c63
池桂宇 <1092369395@qq.com>
2018-07-17 19:17:13 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/leveldb.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chikatsurasakai/leveldb-master/db/builder.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/builder.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/c.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/c.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/db_impl.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/db_impl.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/db_iter.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/db_iter.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/dbformat.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/dbformat.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/dumpfile.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/dumpfile.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/filename.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/filename.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/log_reader.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/log_reader.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/log_writer.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/log_writer.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/memtable.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/memtable.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/repair.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/repair.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/table_cache.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/table_cache.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/version_edit.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/version_edit.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/version_set.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/version_set.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/db/write_batch.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/db/write_batch.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/helpers/memenv/memenv.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/helpers/memenv/memenv.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/block.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/block.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/block_builder.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/block_builder.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/filter_block.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/filter_block.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/format.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/format.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/iterator.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/iterator.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/merger.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/merger.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/table.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/table.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/table_builder.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/table_builder.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/table/two_level_iterator.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/table/two_level_iterator.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/arena.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/arena.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/bloom.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/bloom.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/cache.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/cache.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/coding.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/coding.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/comparator.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/comparator.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/crc32c.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/crc32c.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/env.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/env.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/env_posix.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/env_posix.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/filter_policy.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/filter_policy.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/hash.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/hash.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/logging.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/logging.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/options.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/options.cc.o"
+  "/Users/chikatsurasakai/leveldb-master/util/status.cc" "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/CMakeFiles/leveldb.dir/util/status.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LEVELDB_COMPILE_LIBRARY"
+  "LEVELDB_PLATFORM_POSIX=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,34 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chikatsurasakai/leveldb-master
+# Build directory: /Users/chikatsurasakai/leveldb-master/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(c_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/c_test")
+add_test(fault_injection_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/fault_injection_test")
+add_test(issue178_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/issue178_test")
+add_test(issue200_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/issue200_test")
+add_test(env_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/env_test")
+add_test(status_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/status_test")
+add_test(autocompact_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/autocompact_test")
+add_test(corruption_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/corruption_test")
+add_test(db_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/db_test")
+add_test(dbformat_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/dbformat_test")
+add_test(filename_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/filename_test")
+add_test(log_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/log_test")
+add_test(recovery_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/recovery_test")
+add_test(skiplist_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/skiplist_test")
+add_test(version_edit_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/version_edit_test")
+add_test(version_set_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/version_set_test")
+add_test(write_batch_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/write_batch_test")
+add_test(memenv_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/memenv_test")
+add_test(filter_block_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/filter_block_test")
+add_test(table_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/table_test")
+add_test(arena_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/arena_test")
+add_test(bloom_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/bloom_test")
+add_test(cache_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/cache_test")
+add_test(coding_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/coding_test")
+add_test(crc32c_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/crc32c_test")
+add_test(hash_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/hash_test")
+add_test(logging_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/logging_test")
+add_test(env_posix_test "/Users/chikatsurasakai/leveldb-master/cmake-build-debug/env_posix_test")
